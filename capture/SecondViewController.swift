37c6b294de31a6fd2f80c31e2b561f91ca9a9681
clouie87 <carol.louie@gmail.com>
2015-05-07 16:12:05 -0400

    Initial Commit

---
+++ capture/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  capture
+//
+//  Created by Caroline Louie on 2015-05-07.
+//  Copyright (c) 2015 Caroline Louie. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
